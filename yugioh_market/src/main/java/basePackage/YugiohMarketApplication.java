15dff53bdfe636d9a652acb5440f3a273053d836
Peledew <petrovicluka967@gmail.com>
2023-10-24 17:43:12 +0200

    adding app

---
+++ yugioh_market/src/main/java/basePackage/YugiohMarketApplication.java
@@ -0,0 +1,14 @@
+package basePackage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YugiohMarketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YugiohMarketApplication.class, args);
+		System.out.println("\n\nKOMPAJLIRAM SE!\n\n");
+	}
+
+}
